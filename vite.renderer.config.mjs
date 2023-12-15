998ed0115bed77fbd7d32a6ad128c6b0af12dd6b
junielton <junyellton@gmail.com>
2023-12-15 10:00:00 -0300

    perf: optimize bundle size and performance

---
+++ vite.renderer.config.mjs
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
+import { fileURLToPath } from 'node:url';
+import { dirname, resolve } from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default defineConfig({
+  root: 'src/renderer',
+  base: './',
+  build: {
+    outDir: '../../dist/renderer',
+    emptyOutDir: true,
+  },
+  plugins: [
+    vue(),
+    VueI18nPlugin({
+      include: resolve(__dirname, 'src/renderer/locales/**'),
+      runtimeOnly: true,
+      compositionOnly: true,
+      fullInstall: true
+    })
+  ]
+});
